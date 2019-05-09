7f87802aa99ffbd6db31b66f409b7e65e17de30c
amir yosef <amiryosef99@outlook.com>
2019-05-09 19:09:16 +0200

    add to skills

--- js/main.js
+++ js/main.js
@@ -86,12 +86,12 @@ function myFunction() {
 // Portfolio
 
 
-(function($) {
-    "use strict";
+// (function($) {
+//     "use strict";
 
-    // manual carousel controls
-    $('.next').click(function(){ $('.carousel').carousel('next');return false; });
-    $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
+//     // manual carousel controls
+//     $('.next').click(function(){ $('.carousel').carousel('next');return false; });
+//     $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
 
-})(jQuery);
+// })(jQuery);
 
